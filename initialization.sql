b957f1235018afdeffeb68e6ceb5d282f2e7279c
Zim <zimtsui@gmail.com>
2021-11-24 02:09:19 +0800

    dev

--- initialization.sql
+++ initialization.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS "directory_fnodes_contents" (
 	"child_id"	BIGINT NOT NULL,
 	"btime"	BIGINT NOT NULL,
 	FOREIGN KEY("parent_id") REFERENCES "fnodes_metadata"("id"),
-	PRIMARY KEY("parent_id","child_name"),
-	FOREIGN KEY("child_id") REFERENCES "fnodes_metadata"("id")
+	FOREIGN KEY("child_id") REFERENCES "fnodes_metadata"("id"),
+	PRIMARY KEY("parent_id","child_name")
 );
 DROP TABLE IF EXISTS "regular_file_fnodes_contents";
 CREATE TABLE IF NOT EXISTS "regular_file_fnodes_contents" (
@@ -18,15 +18,15 @@ CREATE TABLE IF NOT EXISTS "regular_file_fnodes_contents" (
 );
 DROP TABLE IF EXISTS "fnodes_metadata";
 CREATE TABLE IF NOT EXISTS "fnodes_metadata" (
-	"id"	BIGINT NOT NULL UNIQUE,
+	"id"	INTEGER NOT NULL UNIQUE,
 	"type"	CHAR(1) NOT NULL,
 	"mtime"	BIGINT NOT NULL,
 	"rmtime"	INTEGER NOT NULL,
 	"previous_version_id"	BIGINT,
 	"first_version_id"	BIGINT NOT NULL,
-	PRIMARY KEY("id"),
+	FOREIGN KEY("first_version_id") REFERENCES "fnodes_metadata"("id"),
 	FOREIGN KEY("previous_version_id") REFERENCES "fnodes_metadata"("id"),
-	FOREIGN KEY("first_version_id") REFERENCES "fnodes_metadata"("id")
+	PRIMARY KEY("id")
 );
 INSERT INTO "fnodes_metadata" ("id","type","mtime","rmtime","previous_version_id","first_version_id") VALUES (1,'d',0,0,NULL,1);
 COMMIT;
